e5e589c0000e8ad0d9f7e4fa5448644b6bc62844
KaterynaOmelchenko <kateomel11@gmail.com>
2020-02-06 21:51:37 -0800

    login.spec.js

---
+++ test/specs/login.spec.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+const Page = require('./Page');
+
+describe('LOGIN', () => {
+    it('should ', () => {
+        Page.open('https://webdriver.io/docs/pageobjects.html');
+    }) ;
+});
